cbaba74b706ccb34f0303d3137de6823534bb32f
Aidan Middleton <aidanemiddleton@gmail.com>
2020-08-13 10:21:51 -0700

    added string examples

---
+++ stringconcatenation.js
@@ -0,0 +1,24 @@
+let intro = 'hi my name is ';
+
+let name = 'Aidan';
+let age = 23;
+
+console.log(intro + name);
+
+console.log(intro + name + ' and I like chicken');
+
+let introAndName = 'hi my name is ' + 'Aidan';
+console.log(introAndName);
+
+let addition = 1 + 5; 
+let stringAddition = intro + name;
+
+console.log(addition);
+console.log(stringAddition);
+
+// backticks example with Template Literals
+
+let bactickString = `hi my name is ${name}, and i am ${age} years old`;
+
+console.log(bactickString);
+
