e22520bf8fd3fe94532251c525185ec0ed263a47
Aidan Middleton <aidanemiddleton@gmail.com>
2020-08-13 09:53:21 -0700

    added equality examples

---
+++ equality.js
@@ -0,0 +1,29 @@
+// = for variable assignemnt. 
+let myVar = 'a word';
+
+console.log(myVar);
+
+console.log('-------------------------------');
+
+// == and != uses type coersion 
+console.log(6 == '6');
+console.log(5 != 9);
+console.log(5 != 5);
+
+console.log('-------------------------------');
+
+// === !==
+console.log(6 === '6');
+console.log('dog' === 'dog');
+let animal = 'dog';
+console.log('dog' === animal);
+console.log(5 !== 5);
+
+
+// >, <, % >=, <=
+
+console.log('is 5 >4? ', 5 > 4);
+console.log('is 5 greater than 5', 5 > 5);
+console.log('is 5 greater than or equal to 5', 5 >= 5);
+console.log('is 5 < 4? ', 5 < 4);
+
